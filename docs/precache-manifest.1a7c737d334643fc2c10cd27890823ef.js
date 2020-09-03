bdc1b9be0e0b69c7916b625eea06bf73d9a8bea6
Alexis Maldonado <alexisalex31@hotmail.com>
2020-09-03 00:02:06 -0500

    docs agregado

---
+++ docs/precache-manifest.1a7c737d334643fc2c10cd27890823ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "312f6216da7bb065cfa0c865468782f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c75fef1507af59059fe",
+    "url": "/static/css/main.7c41cc5b.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c75fef1507af59059fe",
+    "url": "/static/js/main.36a71f4a.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
